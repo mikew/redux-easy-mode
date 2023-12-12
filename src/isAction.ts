367dc932e13fe4229d82aad1ea02d4f9c0791801
Mike Wyatt <wyatt.mike@gmail.com>
2023-12-11 21:08:14 -0400

    simplify isAction

--- src/isAction.ts
+++ src/isAction.ts
@@ -4,21 +4,9 @@ import type { Action } from 'redux'
 // https://github.com/reduxjs/redux/blob/4bdb8aca102f6982b156e1942e200fdc88022eba/src/utils/isAction.ts
 export default function isAction(action: unknown): action is Action<string> {
   return (
-    isPlainObject(action) && 'type' in action && typeof action.type === 'string'
-  )
-}
-
-export function isPlainObject(obj: unknown): obj is object {
-  if (typeof obj !== 'object' || obj == null) {
-    return false
-  }
-
-  let proto = obj
-  while (Object.getPrototypeOf(proto) !== null) {
-    proto = Object.getPrototypeOf(proto)
-  }
-
-  return (
-    Object.getPrototypeOf(obj) === proto || Object.getPrototypeOf(obj) === null
+    action != null &&
+    typeof action === 'object' &&
+    'type' in action &&
+    typeof action.type === 'string'
   )
 }
